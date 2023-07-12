1b1b29ad9c54397426c35a326b078eb31ad87271
WandaSimeoli <wandasimeoli@gmail.com>
2023-07-12 20:35:26 +0200

    Creazione di un nuovo componente

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,23 @@
 <script>
+// Imposrto i componenti 
+import AppTitle from './components/AppTitle.vue'
 // my-component.js
 export default {
   data() {
-    return { count: 0 }
+    return { count: 10 }
   },
-  template: `<div>count is {{ count }}</div>`
+  // Dichiaro i componenti 
+  components : {AppTitle}
 }
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+   {{ count }}
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
+
 </style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data() {
+    return {  }
+  },
+}
+</script>
+
+<template>
+    <header>
+        <h1></h1>
+    </header>
+</template>
+
+<style scoped></style>
